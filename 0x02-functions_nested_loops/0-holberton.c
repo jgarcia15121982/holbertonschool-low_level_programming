cc5c11b85b2287110939884a313103843e3e02a4
jgarcia15121982 <johfregarcia@gmail.com>
2020-02-11 14:44:28 +0000

    Fix: printing Holberton only using _putchar()

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,19 +5,18 @@
  * Return: Always 0.
  */
 
-int main (void)
+int main(void)
 {
-	int i;
-	char a[] = "Holberton\n";
-	char *b;
-	char c;
-	
-	for (i = 0; i <= 9; i++)
-	{
-		b = &a[i];
-		c = *b;
-		_putchar(c);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
 	return (0);
 }

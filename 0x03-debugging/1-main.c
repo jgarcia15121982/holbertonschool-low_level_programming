098af5eda3d962865667a47f4033ea7b30aec235
jgarcia15121982 <johfregarcia@gmail.com>
2020-02-12 22:13:18 +0000

    FIX: Maximum number of files corrected

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,16 +8,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-/**
-*This line was commented to let the program run.
-*\\o/\n: is an special character showed in printf
-*	i = 0;
-*
-*	while (i < 10)
-*	{
-*		putchar(i);
-*	}
-*/
+/*
+ *	i = 0;
+ *
+ *	while (i < 10)
+ *	{
+ *		putchar(i);
+ *	}
+ */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);

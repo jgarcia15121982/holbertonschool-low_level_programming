38aeea72e6ebe0dcce7526ac51b1d99dc44a1d7a
jgarcia15121982 <johfregarcia@gmail.com>
2020-02-12 22:09:22 +0000

    FIX: Maximum number of files corrected

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 /**
-*This line was commented to let
-*the program run.
-*
-*\\o/\n: is an special character showed
-*in printf
-*
+*This line was commented to let the program run.
+*\\o/\n: is an special character showed in printf
 *	i = 0;
 *
 *	while (i < 10)

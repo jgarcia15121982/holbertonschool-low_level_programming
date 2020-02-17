6b72b50d76dfd4d8c4b83b99ba1af50c56875da1
jgarcia15121982 <johfregarcia@gmail.com>
2020-02-17 17:43:21 +0000

    Function that takes a pointer to an int as parameter and updates the value it points to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter
+ * and updates the value it points to 98
+ *
+ * void: it doesn't return a value
+ * @n: a pointer the integer we want to set to 402
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

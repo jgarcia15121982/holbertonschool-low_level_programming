328c024ae4eea92fb23a0fd3844f8c2a6c879e90
jgarcia15121982 <johfregarcia@gmail.com>
2020-02-19 21:24:22 +0000

    Fix: Function corrected

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,34 +10,19 @@
  */
 void print_rev(char *s)
 {
-	int cont = _strlen(s);
+	int cont = 0;
 
-	while (cont >= 0)
+	while (s[cont] != '\0')
 	{
-		_putchar(s[cont]);
+		cont++;
+	}
+
+	while (cont > 0)
+	{
+		_putchar(s[cont - 1]);
 		cont--;
 	}
 
 	_putchar('\n');
 
-}
-
-/**
- * _strlen - returns the length of a string
- *
- * int: return the length of a string
- * @s: a pointer to char s
- *
- * Return: An integer with th length of a string
- */
-int _strlen(char *s)
-{
-	int cont = 0;
-
-	while (*s)
-	{
-		s++;
-		cont++;
-	}
-	return (cont);
 }

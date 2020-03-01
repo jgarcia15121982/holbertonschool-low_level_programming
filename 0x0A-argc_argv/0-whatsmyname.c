670682b180cb7ba535dc3eb9e89638ac8d61ddf6
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-01 21:08:43 +0000

    Fix: Program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 #include "holberton.h"
 /**
  * main - Entry point
- *
+ * @argc: Not used
+ * @argv: Array of size argc
  * Return: Always 0 (Success)
  */
-int main (int UNUSED(argc), char *argv[])
+int main(int UNUSED(argc), char *argv[])
 {
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
-	return 0;
+	return (0);
 }
 

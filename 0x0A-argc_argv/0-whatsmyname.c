eb43259e95394baff7b11d782ffc51bff8a80ad3
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-01 22:37:58 +0000

    Fix: Unused argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,13 @@
 #include "holberton.h"
 /**
  * main - Entry point
- * @argc: Not used
+ * @argc: Size of the argv array
  * @argv: Array of size argc
  * Return: Always 0 (Success)
  */
-int main(int UNUSED(argc), char *argv[])
+int main(int argc, char *argv[])
 {
+	UNUSED(argc);
 	printf("%s\n", argv[0]);
 
 	return (0);

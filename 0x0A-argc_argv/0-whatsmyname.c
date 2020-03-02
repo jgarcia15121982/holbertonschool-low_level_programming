a56a617c757a4c0dfe38818ecb2ff75acf69b940
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-02 01:04:15 +0000

    Fix: Updating issues

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  * @argv: Array of size argc
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int UNUSED(argc), char *argv[])
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
 
 	return (0);

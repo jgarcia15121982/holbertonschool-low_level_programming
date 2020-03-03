9315320b151f84fff1679d6b35687d79398c29fa
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-03 19:52:33 +0000

    Fix: Function output

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,24 +9,28 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *s;
-	unsigned int i;
-
-	if (size == 0)
-		return (NULL);
-
-	s = malloc((size + 1) * sizeof(char));
-	if (s == NULL)
+	if (size > 0)
 	{
-		return (NULL);
-	}
-	i = 0;
+		char *s;
+		unsigned int i;
 
-	while (i < size)
-	{
-		s[i] = c;
-		i++;
+		s = malloc(size * sizeof(*s));
+
+		if (s != NULL)
+		{
+			i = 0;
+
+			while (i < size)
+			{
+				s[i] = c;
+				i++;
+			}
+			return (s);
+		}
+		else
+			return (NULL);
 	}
-	s[i] = '\0';
-	return (s);
+	else if (size == 0)
+		return (NULL);
+	return (0);
 }

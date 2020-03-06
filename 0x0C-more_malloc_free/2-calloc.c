828cc1af14972d974d810b1b8099d432f6e11696
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-06 03:52:34 +0000

    Fix: Validation of nmemb and size equals to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,19 +11,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int i;
 
-	if (nmemb != 0 || size != 0)
-	{
-		p = malloc(size * nmemb + 1);
-
-		if (p != NULL)
-		{
-			for (i = 0; i < nmemb; i++)
-				*(p + i) = '\0';
-			return (p);
-		}
-		else
-			return (NULL);
-	}
-	else
+	if (nmemb == 0 || size == 0)
 		return (NULL);
+	p = malloc(size * nmemb + 1);
+
+	if (!p)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		*(p + i) = '\0';
+	return (p);
 }

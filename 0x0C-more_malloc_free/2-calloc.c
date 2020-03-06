d929ea1ea1776148956c0cdaba1be156c26067c5
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-06 04:19:15 +0000

    Fix: set to 0 conditions

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,11 +13,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(size * nmemb + 1);
+	p = malloc(size * nmemb);
 
 	if (!p)
 		return (NULL);
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i <  size * nmemb; i++)
 		p[i] = 0;
 	return (p);
 }

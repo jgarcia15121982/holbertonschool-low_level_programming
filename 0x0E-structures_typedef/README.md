c976f81296a7a32a3bcbbf58ffe731b2db6dac31
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-09 23:18:06 +0000

    Structures and Typedef

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+# holbertonschool-low_level_programming
+# Structures and Typedef
+## By: John Garcia
+### Structures are fun
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - Define a new type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a new type struct dog with name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

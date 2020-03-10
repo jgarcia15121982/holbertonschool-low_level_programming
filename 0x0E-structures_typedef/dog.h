a495c8df522db184b390974e1b3e11f5b8169e45
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-10 00:30:37 +0000

    Fix: Dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,10 +8,16 @@
  *
  * Description: Define a new type struct dog with name, age and owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif

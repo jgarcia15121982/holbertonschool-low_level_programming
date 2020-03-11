24162058c9d1a40ec07c79ca136564c3c952c65a
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-11 03:59:16 +0000

    Fix: Correct output case when name and owner receives NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,18 +9,21 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		printf("\n");
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age <= 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %.6f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 	else
-		printf("Name: %s\n", d->name);
-	if (d->age <= 0)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %.6f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		printf('\0');
 }

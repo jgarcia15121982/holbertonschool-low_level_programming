e8f8c595a96a2a8f60dff5190a3db08c93af706c
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-11 03:20:23 +0000

    Fix: Function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 /**
  * print_dog - Function that prints a struct dog.
  * @d: Variable of type struct dog.
@@ -6,5 +9,18 @@
  */
 void print_dog(struct dog *d)
 {
-	
+	if (d == NULL)
+		printf("\n");
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %.6f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
 }

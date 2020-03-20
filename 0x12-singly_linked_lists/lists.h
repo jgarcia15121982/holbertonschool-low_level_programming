9fc85518f289aa0efe0e579349fe17402dcdb0ef
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-20 04:59:20 +0000

    [FIX] spaces and betty style

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -12,9 +14,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+#endif

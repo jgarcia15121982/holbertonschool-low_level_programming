a1af25345f06d92807bbb4c9ed2f4171b0de2c22
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-03 22:44:23 +0000

    [ADD] Get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Function that returns a node
+ * indicated by a given index
+ * @head: The head node
+ * @index: index that identifies the node
+ *
+ * Return: Node found by the index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+	for (i = 0; i < index; i++, head = head->next)
+		if (!head)
+			return (NULL);
+        return (head);
+}

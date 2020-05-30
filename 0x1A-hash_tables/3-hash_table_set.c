f7b867adbafc835a795954e49a5f9f9b40a583da
jgarcia15121982 <johfregarcia@gmail.com>
2020-05-30 01:28:32 +0000

    [ADD]  function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Function that adds an element to the hash table
+ * @ht: The hash table for add or update the key/value
+ * @key: The key. Cannot be an empty string
+ * @value: Is the value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *tmp, *new_ht;
+
+	if (ht == NULL)
+		return (0);
+
+	if (key == NULL)
+		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	new_ht = malloc(sizeof(hash_node_t));
+	if (new_ht == NULL)
+		return (0);
+	new_ht->key = strdup(key);
+	new_ht->value = strdup(value);
+	if (new_ht->value == NULL || new_ht->key == NULL)
+		return (0);
+	if (ht->array[idx])
+	{
+		for (tmp = ht->array[idx]; tmp; tmp = tmp->next)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				free(new_ht->key);
+				free(new_ht->value);
+				free(new_ht);
+				free(tmp->value);
+				if (tmp->value == NULL)
+					return (0);
+				return (1);
+			}
+		}
+		new_ht->next = ht->array[idx];
+		ht->array[idx] = new_ht;
+	}
+	else
+	{
+		new_ht->next = NULL;
+		ht->array[idx] = new_ht;
+	}
+	return (1);	
+}

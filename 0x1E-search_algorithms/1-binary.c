3e67bb6ad827aa246288db0365f3d722c4aabdb7
jgarcia15121982 <johfregarcia@gmail.com>
2020-08-26 21:36:25 -0500

    [ADD] Binary search file

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+/**
+ * binary_search - function that searches for
+ * a value in an array of integers using the
+ * binary search algorithm.
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: index on success, otherwise -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int l, r, m;
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	l = 0;
+	r = size - 1;
+
+	while (l <= r)
+	{
+		printf("Searching in array: ");
+		for (i = l; i <= (size_t)r; i++)
+		{
+			printf("%d", array[i]);
+			if (i != (size_t)r)
+				printf(", ");
+		}
+		printf("\n");
+		m = ((l + r) / 2);
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
+}
